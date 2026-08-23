d3cdc401d6eb16c5bb3c5e3e2e09a5e484ae79e0
Dharshini Suresh <sdharshini1310@gmail.com>
2026-08-23 11:29:03 +0530

    Add star triangle class problem

---
+++ src/main/java/oop/class_problems/RightAngledStarTriangle.java
@@ -0,0 +1,20 @@
+package oop.class_problems;
+
+import java.util.Scanner;
+
+public class RightAngledStarTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println("The right-angled triangle pattern for " + rows + " rows is");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
