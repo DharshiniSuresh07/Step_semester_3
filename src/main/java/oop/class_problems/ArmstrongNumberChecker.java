8bb3480d5ed21db4407d06afd518a1748467862b
Dharshini Suresh <sdharshini1310@gmail.com>
2026-08-23 11:36:17 +0530

    Add Armstrong number class problem

---
+++ src/main/java/oop/class_problems/ArmstrongNumberChecker.java
@@ -0,0 +1,20 @@
+package oop.class_problems;
+import java.util.Scanner;
+public class ArmstrongNumberChecker {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int origNumber = number;
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            sum = sum + digit * digit * digit;
+            number = number / 10;
+        }
+        boolean isArmstrong = sum == origNumber;
+        System.out.println("Is the number " + origNumber +
+                " an Armstrong number? " + isArmstrong);
+        sc.close();
+    }
+}
